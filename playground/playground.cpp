42a8922a8aea805608a9a930a8c6b966526ffe22
D3baka <92331049+D3baka@users.noreply.github.com>
2022-11-10 08:20:07 +0100

    Added scaleability + code cleanup

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -34,13 +34,13 @@ public:
     GLuint vertexbuffer_size;
     glm::mat4 translation;
     virtual void update(glm::mat4* mvp) = 0;
-    void draw() {
+    void draw(float scale) {
 
         glm::mat4 mvp_matrix = translation;
         mvp_matrix = mvp_matrix * glm::mat4(
-            0.5, 0, 0, 0,
-            0, 0.5, 0, 0,
-            0, 0, 0.5, 0,
+            scale, 0, 0, 0,
+            0, scale, 0, 0,
+            0, 0, scale, 0,
             0, 0, 0, 1
         );
 
@@ -85,7 +85,7 @@ public:
 
         glDisableVertexAttribArray(0);
     }
-    bool initializeColorbuffer()
+    virtual bool initializeColorbuffer()
     {
         static GLfloat g_color_buffer_data[18];
         g_color_buffer_data[0] = 1;
@@ -125,7 +125,7 @@ public:
         return true;
     }
 
-    bool initializeVertexbuffer()
+    virtual bool initializeVertexbuffer()
     {
 
 
@@ -223,10 +223,66 @@ class Player : public GameObject{
 
             //std::cout << test[0][3] << std::endl;
             //std::cout << test[1][3] << std::endl;
-            draw();
+            draw(0.5f);
 
 			
         }
+		bool initializeVertexbuffer() override {
+
+            glGenVertexArrays(1, &VertexArrayID);
+            glBindVertexArray(VertexArrayID);
+
+
+
+            vertexbuffer_size = 3;
+            glm::vec2 triangleVertice1 = glm::vec2(-1.0f, -1.0f);
+            glm::vec2 triangleVertice2 = glm::vec2(1.0f, -1.0f);
+            glm::vec2 triangleVertice3 = glm::vec2(0.0f, 1.0f);
+         
+            static GLfloat g_vertex_buffer_data[9];
+            g_vertex_buffer_data[0] = triangleVertice1[0];
+            g_vertex_buffer_data[1] = triangleVertice1[1];
+            g_vertex_buffer_data[2] = 0.0f;
+
+            g_vertex_buffer_data[3] = triangleVertice2[0];
+            g_vertex_buffer_data[4] = triangleVertice2[1];
+            g_vertex_buffer_data[5] = 0.0f;
+
+            g_vertex_buffer_data[6] = triangleVertice3[0];
+            g_vertex_buffer_data[7] = triangleVertice3[1];
+            g_vertex_buffer_data[8] = 0.0f;
+
+            glGenBuffers(1, &vertexbuffer);
+            glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+            return true;
+			
+		}
+
+        bool initializeColorbuffer() override {
+            static GLfloat g_color_buffer_data[9];
+            g_color_buffer_data[0] = 1;
+            g_color_buffer_data[1] = 1;
+            g_color_buffer_data[2] = 1;
+
+            g_color_buffer_data[3] = 1;
+            g_color_buffer_data[4] = 1;
+            g_color_buffer_data[5] = 1;
+
+            g_color_buffer_data[6] = 1;
+            g_color_buffer_data[7] = 1;
+            g_color_buffer_data[8] = 1;
+			
+            glDeleteBuffers(1, &colorbuffer);
+            glGenBuffers(1, &colorbuffer);
+            glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+			
+            return true;
+        }
+
+		
 };
 
 class Enemy : public GameObject{
@@ -240,7 +296,7 @@ class Enemy : public GameObject{
         }
         void update(glm::mat4* mvp) override {
             
-            draw();
+            draw(1.0f);
 
 
         }
@@ -257,7 +313,7 @@ class Projectile : public GameObject{
         }
         void update(glm::mat4* mvp) override {
             translation = translation;
-            draw();
+            draw(0.1f);
 
 
         }
@@ -276,8 +332,8 @@ int enemyCount;
 
 
 //define size of screens
-int screenWidth = 800;
-int screenHeight = 720;
+int screenWidth = 1066;
+int screenHeight = 960;
 glm::ivec2  canvasSize = glm::ivec2(screenWidth, screenHeight);
 double stretchfactor = double(screenWidth) / double(screenHeight);
 
@@ -372,31 +428,17 @@ void updateAnimationLoop()
     //Analyze User Inputs
     if (glfwGetKey(window, GLFW_KEY_W)) {
         y += 0.003f;
-        
-
-        
     }
 
     if (glfwGetKey(window, GLFW_KEY_S)) {
-        
-
-        y += -0.003f;
-        
-       
+        y += -0.003f;       
     }
     if (glfwGetKey(window, GLFW_KEY_D)) {
-        
-        
-        
         x += 0.003f;
     }
     if (glfwGetKey(window, GLFW_KEY_A)) {
-        
-       
-        
         x += -0.003f;
     }
-    
     
     mvp_matrix = glm::mat4(
         1, 0, 0, x,
@@ -406,25 +448,14 @@ void updateAnimationLoop()
     );
     
 	
-    
     glClear(GL_COLOR_BUFFER_BIT);
 
     for (int i = 0; i < gameObjects.size(); i++) {
-       
-
-
-        // Clear the screen
-
         gameObjects[i]->update(&mvp_matrix);
-        //std::cout << mvp_matrix[0][3] << std::endl;
-        //std::cout << mvp_matrix[1][3] << std::endl;
-        
     }
 
   
     
-	//scaling the final result
-   
   glDisableVertexAttribArray(1);
 
 
@@ -472,8 +503,8 @@ bool initializeWindow()
   // Ensure we can capture the escape key being pressed below
   glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-  // White background
-  glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+  // Black background
+  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
   return true;
 }
 
